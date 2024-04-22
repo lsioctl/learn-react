51e844eecf08e48fab4d6d2303cea4cd71f15ddd
lsioctl <lsioctl@gmail.com>
2024-04-22 15:33:59 -0400

    added pagination

--- nextjs-dashboard/app/lib/debounce.ts
+++ nextjs-dashboard/app/lib/debounce.ts
@@ -1,11 +1,9 @@
 
 export default function debounce(fn: Function, delay: number) {
   let timeoutId: null | ReturnType<typeof setTimeout> = null
-  console.log(arguments);
 
   return (...args: any[]) => {
       if (timeoutId !== null) {
-        console.log("clearing");
         clearTimeout(Number(timeoutId));
       }
 
