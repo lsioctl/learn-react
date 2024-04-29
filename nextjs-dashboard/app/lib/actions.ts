c1fe50bdf1393a3ba9d3e6d490bd74ec8d5ca0bc
lsioctl <lsioctl@gmail.com>
2024-04-29 10:49:35 -0400

    delete invoice

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -40,3 +40,15 @@ export async function createInvoice(formData: FormData) {
 
   redirect('/dashboard/invoices');
 }
+
+export async function deleteInvoice(id: string) {
+  // throw new Error('Failed to Delete Invoice');
+
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return { message: 'Deleted Invoice' };
+  } catch (error) {
+    return { message: 'Database Error: Failed to Delete Invoice' };
+  }
+}
